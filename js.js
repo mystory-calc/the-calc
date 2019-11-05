b6d5e30ddd055b34451485c672aa6df25252b110
muftah <muftah.elzawi@gmail.com>
2019-11-05 10:39:06 +0100

    firstNumber, secondNumber and opO functions

--- js.js
+++ js.js
@@ -1,4 +1,26 @@
 
-$('#numbers').on('click', function(event){
-	console.log($(event.target).val())
+var firstNumbers = [];
+var secondNumbers = [];
+var opO = [];
+var checker = 'first';
+
+
+$('.num-btn').on('click', function(event){
+	if (checker === 'first') {
+ 		firstNumbers.push($(event.target).val())
+ 	console.log(firstNumbers)
+ 	}else if (checker === 'second'){
+ 		secondNumbers.push($(event.target).val())
+	console.log(secondNumbers)
+ 	}
 })
+
+$('.op-btn').on('click', function(event){
+	opO.push($(event.target).val())
+	console.log(opO)
+	checker = 'second'
+	console.log(checker)
+})
+
+
+
