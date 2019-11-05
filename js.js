a9a342811312f0693cc3c954e8566669bb39174e
muftah <muftah.elzawi@gmail.com>
2019-11-05 13:50:06 +0100

    completed all the basics

--- js.js
+++ js.js
@@ -7,29 +7,40 @@ var dis = $('#display-opration')
 
 $('#img').hide()
 $('.num-btn').on('click', function(event){
+console.log(firstNumbers, 'first')
+console.log(secondNumbers , "second")
+console.log(checker)
 	if (checker === 'first') {
+
  		firstNumbers.push($(event.target).val())
  		dis.text(parseInt(firstNumbers.join('')))
- 	console.log(firstNumbers)
+ 		console.log(firstNumbers)
+
  	}else if (checker === 'second'){
+
  		secondNumbers.push($(event.target).val())
-	console.log(secondNumbers)
-	dis.text( parseInt(secondNumbers.join('')))
+		console.log(secondNumbers)
+		dis.text( parseInt(secondNumbers.join('')))
+
  	}
  	
 
 })
 
 $('.op-btn').on('click', function(event){
+
 	opO = $(event.target).val();
 	checker = 'second'
 	dis.text(opO)
+
 })
 
 function equal(firstNumbers, opO, secondNumbers){
+
 	var firstNumber = parseInt(firstNumbers.join(''));
 	var secondNumber = parseInt(secondNumbers.join(''));
 	var result = 0;
+
 	switch(opO){
 		case '+': 
 		result = firstNumber + secondNumber;
@@ -44,17 +55,39 @@ function equal(firstNumbers, opO, secondNumbers){
 		if (secondNumber === 0) {
 			console.log("we don't do that here");
 			$('#img').show();
+
 		}else {
-			result = firstNumber / secondNumber;		
+			result = firstNumber / secondNumber;
+
 		}
 		break;	
 	}
+
 	dis.text(result);
+	return result
 }
 
 $('#equal-sign').on('click', function(){
-	equal(firstNumbers, opO, secondNumbers);
+
+	var outCome = equal(firstNumbers, opO, secondNumbers);
+	clearAll();
+	firstNumbers.push(outCome);
+	checker = 'second';
+
 })
 
+function clearAll() {
 
+	firstNumbers = [];
+	secondNumbers = [];
+	checker = 'first';
+	opO = '';
 
+}
+
+$('#clear-display').on('click',function () {
+
+	clearAll();
+	dis.text(0);
+
+})
